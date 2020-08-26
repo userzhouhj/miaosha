ae1f74f38d6d1d8b022a08b340859c493503a35a
userzhouhj <1125450759@qq.com>
2020-08-26 14:44:35 +0800

    首次提交

---
+++ order/src/main/java/com/jun/order/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.jun.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：userzhou
+ * @date ：Created in 2020
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
---
+++ seckill/src/main/java/com/jun/seckill/SecKillApplication.java
@@ -0,0 +1,16 @@
+package com.jun.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：userzhou
+ * @date ：Created in 2020
+ */
+@SpringBootApplication
+public class SecKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecKillApplication.class,args);
+    }
+}
